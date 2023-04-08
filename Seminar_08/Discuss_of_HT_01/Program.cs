e6a754c377454c8dcc3b38d7c487234999f301f9
SMV <kraft088@gmail.com>
2023-04-08 19:02:22 +1100

    Add Discuss of HW

---
+++ Seminar_08/Discuss_of_HT_01/Program.cs
@@ -0,0 +1,87 @@
+﻿// Обсуждение домашнего задания к Семинару №6:
+
+// Задача 41:
+// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+// МОЙ Вариант решения:
+
+// int HowManyNumbersYouWant(string message)
+// {
+//     System.Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int howMany = HowManyNumbersYouWant("Введите требуемое кол-во чисел: ");
+
+// int[] numbers = new int[howMany];
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     numbers[i] = HowManyNumbersYouWant($"Введите число {i + 1}: ");
+// }
+
+// Console.Write("{0}", string.Join(", ", numbers) + " -> ");
+
+// int HowManyNumbersHigherZero(int[] numbers)
+// {
+//     int count = 0;
+//     for (int i = 0; i < numbers.Length; i++)
+//     {
+//         if (numbers[i] > 0)
+//         {
+//             count += 1;
+//         }
+//     }
+//     return count;
+// }
+
+// System.Console.WriteLine(HowManyNumbersHigherZero(numbers));
+
+
+// Вариант решения преподавателя:
+
+int InputNumberWithMessage(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = InputNumberWithMessage($"Введите {i + 1} число: ");
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            count = count + 1;
+
+    }
+    return count;
+}
+
+int length = InputNumberWithMessage("Введите количество чисел: ");
+int[] nunbers = new int[length];
+
+FillArray(nunbers);
+PrintArray(nunbers);
+
+int result = CountPositiveNumbers(nunbers);
+
+System.Console.WriteLine();
+System.Console.WriteLine($"Количество положительных чисел: {result}");
---
+++ Seminar_08/Discuss_of_HT_02/Program.cs
@@ -0,0 +1,47 @@
+﻿// Обсуждение домашнего задания к Семинару №6:
+
+// Задача 43: 
+// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// МОЙ Вариант решения:
+
+// System.Console.Write("Введите точку b1: ");
+// var b1 = Convert.ToDouble(Console.ReadLine());
+// System.Console.Write("Введите точку k1: ");
+// var k1 = Convert.ToDouble(Console.ReadLine());
+// System.Console.Write("Введите точку b2: ");
+// var b2 = Convert.ToDouble(Console.ReadLine());
+// System.Console.Write("Введите точку k2: ");
+// var k2 = Convert.ToDouble(Console.ReadLine());
+
+// var x = -(b1 - b2) / (k1 - k2);
+// var y = k1 * x + b1;
+
+// x = Math.Round(x, 3);
+// y = Math.Round(y, 3);
+
+// System.Console.WriteLine($"Пересечение в точке: ({x};{y})");
+
+
+// Вариант решения преподавателя:
+
+double InputNumberWithMessage(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double k1 = InputNumberWithMessage("Введите к1: ");
+double b1 = InputNumberWithMessage("Введите b1: ");
+
+double k2 = InputNumberWithMessage("Введите k2: ");
+double b2 = InputNumberWithMessage("Введите b2: ");
+
+double x = (b2 - b1) / (k1 - k2);
+double y = x * k1 + b1;
+
+System.Console.WriteLine("Координаты точки пересечения:");
+System.Console.WriteLine($"x = {x}");
+System.Console.WriteLine($"y = {y}");
